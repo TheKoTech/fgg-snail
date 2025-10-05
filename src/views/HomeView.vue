39046d6621dda8ad8f3d4e548cdf9742a9cc6cb6
Welnyr <thekotech@gmail.com>
2025-10-06 00:55:07 +0500

    feat: Add pinia and routing

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useAuthStore } from '../stores/authStore'
+
+const storeValue = useAuthStore
+</script>
+
+<template>
+	<span>HomeView</span>
+
+	<RouterLink to="/login">aboba</RouterLink>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+	<span>LoginView</span>
+
+	<RouterLink to="/">aboba</RouterLink>
+</template>
